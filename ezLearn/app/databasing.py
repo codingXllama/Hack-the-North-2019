9b9f2746969431a272ad42b1c9e1a54c5c3bcada
Vraj Prajapati <48883986+vproHacks@users.noreply.github.com>
2019-09-14 02:07:36 -0400

    Inserted Properly Formatted Flask Backend

---
+++ ezLearn/app/databasing.py
@@ -0,0 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
+
+from app import app #Change this based on the application
+
+db = SQLAlchemy(app)
+
+class account(db.Model):
+    db.Column('id', db.Integer, primary_key=True)
+    db.Column('username', db.String(48))
+    db.Column('password', db.String(64))
+    db.Column('account_id', db.String(24))
+    # Maybe add names and more personal stuff later
+
+class image(db.Model):
+    db.Column('path', db.String(200))
+    db.Column('name', db.String(64))
+    db.Column('crop_height', db.Integer())
+    db.Column('crop_width', db.Integer())
+    db.Column('offset_x', db.Integer())
+    db.Column('offset_y', db.Integer())
+
+class document(db.Model):
+    db.Column('path', db.String(200))
+    db.Column('name', db.String(64))
+
